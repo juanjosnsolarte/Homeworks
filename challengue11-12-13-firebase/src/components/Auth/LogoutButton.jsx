c24a4cf90c70e78ac0ccb063c95ad1d9487dd8e6
Juan Solarte <juan.josn_solarte@uao.edu.co>
2025-10-29 16:00:29 -0500

    Challengue10

--- challengue11-12-13-firebase/src/components/Auth/LogoutButton.jsx
+++
@@ -1,7 +0,0 @@
-import { useDispatch } from 'react-redux';
-import { startLogout } from '../../store/thunks/authThunks';
-
-export default function LogoutButton() {
-  const dispatch = useDispatch();
-  return <button className="btn" onClick={()=>dispatch(startLogout())}>Salir</button>;
-}
--- challengue7-lists/src/data/mockSongs.js
+++
@@ -1,5 +0,0 @@
-export const mockSongs = [
-  { title: "EoO", artist: "Bad Bunny" },
-  { title: "Moscow Mule ", artist: "Bad Bunny" },
-  { title: "120", artist: "Bad Bunny" }
-];
