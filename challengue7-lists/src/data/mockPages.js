5b99e5861c89c88ee2858f75d1c969c95de1d5dd
Juan Solarte <juan.josn_solarte@uao.edu.co>
2025-10-29 15:48:58 -0500

    Challengue 8

--- challengue7-lists/src/data/mockPages.js
+++
@@ -1,7 +0,0 @@
-const mockPages = [
-  { title: "UAO Virtual", url: "https://campus.uaovirtual.edu.co" },
-  { title: "Youtube", url: "https://www.youtube.com" },
-  { title: "Portal UAO", url: "https://apps2.uao.edu.co" }
-];
-
-export default mockPages;
